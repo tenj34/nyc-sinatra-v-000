a33fbf8f0b97fda623ac0d9e0693d09a4f291590
Juan Tenezaca <tenj34@gmail.com>
2018-10-23 21:52:24 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,7 +2,7 @@ class FiguresController < ApplicationController
   # add controller methods
   get '/figures' do
     @figures = Figure.all
-     erb :'figures/index'
+    erb :'figures/index'
   end
 
   get '/figures/new' do
